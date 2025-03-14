597f32ab12770fa809bb7e281101d9cf5c4eca37
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-13 23:50:57 -0500

    template done

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import http from "http"
 
 const server = http
   .createServer((req, res) => {
-    res.statusCode = 200
-    res.end("Hello world")
+    if (req.url === "/") {
+      // redirtec to index
+    } else if (req.url === "/about") {
+      //about
+    } else if (req.url === "/contact-me") {
+      //redirect to contact
+    } else {
+      //redirect to 404
+    }
   })
-  .listen(3000)
+  .listen(8080)
